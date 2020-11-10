5028502d5442c876b3a0d6dfdf9affbccdd22775
Edzus Milasus <a01409784@unet.univie.ac.at>
2020-11-10 22:45:41 +0100

    setting up notification bolts

---
+++ src/main/java/bolts/notifyLeavingAreaBolt.java
@@ -0,0 +1,44 @@
+package bolts;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class notifyLeavingAreaBolt extends BaseRichBolt {
+
+    OutputCollector outputCollector;
+    Map<Integer, Integer> idNotificationMap;
+
+    @Override
+    public void prepare(Map<String, Object> map, TopologyContext topologyContext,
+        OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+        idNotificationMap = new HashMap<Integer, Integer>();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        //0 ... id of the notification
+        //1 ... id of the taxi
+        int idNotification = tuple.getInteger(0);
+        int idTaxi = tuple.getInteger(1);
+
+        if(!idNotificationMap.containsKey(idNotification)){
+            idNotificationMap.put(idNotification, idTaxi);
+
+            System.out.println("Taxi " + idTaxi + " is leaving a predefined area, implement http notification!");
+            //TODO: implement frontend notification
+        }
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        //there is only output to the frontend
+    }
+}
---
+++ src/main/java/bolts/notifySpeedingBolt.java
@@ -0,0 +1,43 @@
+package bolts;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class notifySpeedingBolt extends BaseRichBolt {
+
+    OutputCollector outputCollector;
+    Map<Integer, Integer> idNotificationMap;
+
+    @Override
+    public void prepare(Map<String, Object> map, TopologyContext topologyContext,
+        OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+        idNotificationMap = new HashMap<Integer, Integer>();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        //0 ... id of the notification
+        //1 ... id of the taxi
+        int idNotification = tuple.getInteger(0);
+        int idTaxi = tuple.getInteger(1);
+
+        if(!idNotificationMap.containsKey(idNotification)){
+            idNotificationMap.put(idNotification, idTaxi);
+
+            System.out.println("Taxi " + idTaxi + " is speeding, implement http notification!");
+            //TODO: implement frontend notification
+        }
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        //there is only output to the frontend
+    }
+}
