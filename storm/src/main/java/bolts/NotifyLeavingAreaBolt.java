0237fdd65b95aadfe54ce8f4f2e37d1eef99f7da
bliezejs <edzusmi@gmail.com>
2021-01-12 16:43:19 +0100

    Fixed NotificationLeavingAreaBolt wrong parentheses

--- storm/src/main/java/bolts/NotifyLeavingAreaBolt.java
+++ storm/src/main/java/bolts/NotifyLeavingAreaBolt.java
@@ -23,12 +23,12 @@ public class NotifyLeavingAreaBolt extends BaseRichBolt {
     private Logger logger;
 
 
-    private Double latitudeBeijing = 39.9075;
-    private Double longitudeBeijing = 116.39723;
+    private Double latitudeBeijing = 39.9035;
+    private Double longitudeBeijing = 116.40048;
 
     private TaxiLog centerBeijingLocation;
 
-    private Integer maxDistanceToBeijingCenterKiloMeter = 10;
+    private Integer maxDistanceToBeijingCenterKiloMeter = 100;
 
     
     private WebsocketClientEndpoint clientEndPoint;
@@ -72,15 +72,33 @@ public class NotifyLeavingAreaBolt extends BaseRichBolt {
         Double distanceToBeijingCenterMeter = 0.;
         distanceToBeijingCenterMeter = CoordinateHelper.calculateDistance(currentLog, centerBeijingLocation);
 
+        this.logger.log("Timestamp current log: " + currentLog.getTimestamp());
+
+        this.logger.log("lastlogs from notify leaving: " + lastLogs.toString());
+
         if (!lastLogs.containsKey(taxiId)) {
+            
+            this.logger.log("last logs does not contain taxiId");
             if (distanceToBeijingCenterMeter > maxDistanceToBeijingCenterKiloMeter) {
+
                 this.logger.log("Taxi " + taxiId + " is leaving a predefined area!");
 
                 this.lastLogs.put(taxiId, currentLog);
                 
+
+                this.logger.log("Distance calculated: " + distanceToBeijingCenterMeter + ", current coordinates taxi: " + 
+                    currentLog.getLatitude() + ", " + currentLog.getLongitude()+ ", Beijing Center: " + 
+                    centerBeijingLocation.getLatitude() + ", " + centerBeijingLocation.getLongitude());
+
                 sendLeavingAreaMessageToDashboard(true, taxiId);
-            } else {
+
+            } 
+        }else {
+                
+                this.logger.log("last logs contains taxiId");
                 TaxiLog existingLog = this.lastLogs.get(taxiId);
+
+                this.logger.log("Existing log timestamp: " + existingLog.getTimestamp());
 
                 if (distanceToBeijingCenterMeter <= maxDistanceToBeijingCenterKiloMeter &&
                     existingLog.getTimestamp() <= currentLog.getTimestamp()) {
@@ -94,7 +112,7 @@ public class NotifyLeavingAreaBolt extends BaseRichBolt {
             }
 
         }
-    }
+    
         
     
 
