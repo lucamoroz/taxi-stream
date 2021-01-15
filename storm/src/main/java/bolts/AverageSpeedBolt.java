f4c11fbbd57f41b781b1a0fe9eb7e57225097532
Luca <lucamoro96@gmail.com>
2021-01-15 18:29:10 +0100

    Optimized AverageSpeedBolt

--- storm/src/main/java/bolts/AverageSpeedBolt.java
+++ storm/src/main/java/bolts/AverageSpeedBolt.java
@@ -3,6 +3,7 @@ package bolts;
 import org.apache.storm.redis.bolt.AbstractRedisBolt;
 import org.apache.storm.redis.common.config.JedisClusterConfig;
 import org.apache.storm.redis.common.config.JedisPoolConfig;
+import org.apache.storm.streams.Pair;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -10,13 +11,12 @@ import org.apache.storm.tuple.Tuple;
 import redis.clients.jedis.JedisCommands;
 import utils.Logger;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AverageSpeedBolt extends AbstractRedisBolt {
-    Map<Integer, List<Double>> lastSpeeds = new HashMap<>();
+    // Pair contains the last average speed and the total number of samples
+    Map<Integer, Pair<Double, Integer>> lastDataMap = new HashMap<>();
     Logger logger;
 
     public AverageSpeedBolt(JedisPoolConfig config) {
@@ -38,26 +38,19 @@ public class AverageSpeedBolt extends AbstractRedisBolt {
         int taxiId = input.getIntegerByField("id");
         double speed = input.getDoubleByField("speed");
 
-        // todo avg speed can be computed with constant memory usage, see:
-        //  https://math.stackexchange.com/questions/106700/incremental-averageing
-        List<Double> speeds;
 
-        if (lastSpeeds.containsKey(taxiId)) {
-            speeds = lastSpeeds.get(taxiId);
-        }else{
-            speeds = new ArrayList();
-            lastSpeeds.put(taxiId, speeds);
-        }
 
-        speeds.add(speed);
-        double averageSpeed = speeds.stream().reduce(0d, Double::sum) / speeds.size();
+        Pair<Double, Integer> lastData = lastDataMap.getOrDefault(taxiId, Pair.of(0., 0));
 
+        final double newAvgSpeed = lastData.value1 + ((speed - lastData.value1) / (double)(lastData.value2 + 1));
+        Pair<Double, Integer> newData = Pair.of(newAvgSpeed, lastData.value2 + 1);
+        lastDataMap.put(taxiId, newData);
 
         JedisCommands jedisCommands = null;
         try {
             jedisCommands = getInstance();
-            jedisCommands.hset(String.valueOf(taxiId), "average_speed", String.format("%.6f", averageSpeed));
-            logger.log(String.format("average speed of taxi %d: %.2f km/h", taxiId, averageSpeed));
+            jedisCommands.hset(String.valueOf(taxiId), "average_speed", String.format("%.6f", newAvgSpeed));
+            logger.log(String.format("average speed of taxi %d: %.2f km/h", taxiId, newAvgSpeed));
         } finally {
             if (jedisCommands != null) {
                 returnInstance(jedisCommands);
