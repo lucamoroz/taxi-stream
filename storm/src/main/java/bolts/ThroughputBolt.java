71334bc52e520de1ad59ebe2315f47c823b23ed0
Luca <lucamoro96@gmail.com>
2021-01-16 19:20:26 +0100

    average throughput + current throughput fix

--- storm/src/main/java/bolts/ThroughputBolt.java
+++ storm/src/main/java/bolts/ThroughputBolt.java
@@ -5,9 +5,7 @@ import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
-import org.apache.storm.tuple.Values;
 import utils.Logger;
-import utils.Numbers;
 
 import java.util.Map;
 
@@ -16,6 +14,8 @@ public class ThroughputBolt extends BaseRichBolt {
     Logger logger;
     long lastThroughputMeasurementNs = 0;
     long nProcessedTuplesAccumulator = 0;
+    double averageThroughput = 0;
+    long nThroughputSamples = 0;
 
     @Override
     public void prepare(Map<String, Object> topoConf, TopologyContext context, OutputCollector collector) {
@@ -29,10 +29,19 @@ public class ThroughputBolt extends BaseRichBolt {
 
         // show throughput every 1 second
         if ((System.nanoTime() - lastThroughputMeasurementNs) > 1000000000) {
-            logger.log(String.format("Throughput: %d", nProcessedTuples));
+            // Note: with 1 second cadence nProcessedTuplesAccumulator == current throughput
+            double numerator = ((double) nProcessedTuplesAccumulator) - averageThroughput;
+            double denominator = (double) (nThroughputSamples+1);
+            averageThroughput = averageThroughput + (numerator / denominator);
+            logger.log(String.format("Current throughput: %d, Average Throughput: %.3f", nProcessedTuplesAccumulator, averageThroughput));
+
             nProcessedTuplesAccumulator = 0;
+            nThroughputSamples++;
             lastThroughputMeasurementNs = System.nanoTime();
+        } else {
+            nProcessedTuplesAccumulator += nProcessedTuples;
         }
+        _collector.ack(input);
     }
 
     @Override
